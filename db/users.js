6f6d6aad132a9c491b1fdef217517b72b64f7771
Rehan Fazal <rf26538@gmail.com>
2025-12-16 00:08:19 +0530

    passport implimented

--- db/users.js
+++ db/users.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose').default;
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  profileImage: String,
-  contact: String,
-  boards: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Board"
-  }]
-});
+const userSchema = mongoose.Schema({
+  username : String,
+  name : String,
+  email : String,
+  password : String,
+  profileImage : String,
+  contact : Number,
+  boards : {
+    type : Array,
+    default : []
+  }
+})
 
 userSchema.plugin(plm);
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("user", userSchema)
