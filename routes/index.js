0b12b6ceed9db2162a71deb18f164ce5b7c21724
Rehan Fazal <rf26538@gmail.com>
2025-12-16 21:02:59 +0530

    All pages added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require("../db/users");
+const postModel = require("../db/post");
 const passport = require('passport');
 const localStrategy = require("passport-local");
 const upload = require("../utils/multer");
@@ -15,10 +16,44 @@ router.get('/register', (req, res, next) => {
   res.render('register', { nav: false });
 });
 
-
 router.get('/profile', isLoggedIn, async (req, res, next) => {
-  const user = await userModel.findOne({username : req.session.passport.user});
+  const user = await userModel
+          .findOne({username : req.session.passport.user})
+          .populate("posts");
   res.render('profile', {user, nav :true});
+});
+
+router.get('/show/posts', isLoggedIn, async (req, res, next) => {
+  const user = await userModel
+          .findOne({username : req.session.passport.user})
+          .populate("posts");
+  res.render('show', {user, nav :true});
+});
+
+router.get('/feed', isLoggedIn, async (req, res, next) => {
+  const user = await userModel.findOne({ username: req.session.passport.user });
+  const posts = await postModel.find().populate("user");
+  res.render('feed', { user, posts, nav: true });
+});
+
+
+router.get('/add', isLoggedIn, async (req, res, next) => {
+  const user = await userModel.findOne({username : req.session.passport.user});
+  res.render('add', {user, nav :true});
+});
+
+router.post('/createpost', isLoggedIn, upload.single("postimage"), async (req, res, next) => {
+  const user = await userModel.findOne({username : req.session.passport.user});
+  const post = await postModel.create({
+    user : user._id,
+    title : req.body.title,
+    description : req.body.description,
+    image : req.file.filename
+  });
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile")
 });
 
 router.post('/fileupload', isLoggedIn, upload.single("image"), async (req, res, next) => {
